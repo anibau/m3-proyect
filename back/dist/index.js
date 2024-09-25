dfd4012358ed22da73836f7b71b83289bf776fd0
anibau <miriambt678@gmail.com>
2024-09-25 18:46:28 -0500

    HW-2-configuracion

---
+++ back/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+const usuario1 = {
+    nombres: 'miriam anelis',
+    apellidos: 'bautista torres',
+    edad: 32,
+    email: 'miriam@gmail.com',
+};
+console.log(usuario1);
---
+++ back/src/index.ts
@@ -0,0 +1,14 @@
+interface IUser{
+    nombres: string,
+    apellidos: string,
+    edad: number,
+    email: string,
+}
+const usuario1: IUser={
+    nombres: 'miriam anelis',
+    apellidos: 'bautista torres',
+    edad: 32,
+    email: 'miriam@gmail.com',
+}
+
+console.log(usuario1)
